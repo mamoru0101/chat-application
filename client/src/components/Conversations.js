aaf7a60dd424de2ffc4a493788b73bb207978164
kdm0101 <d08020386730@gmail.com>
2021-07-15 08:55:29 +0900

    conversation components

--- client/src/components/Conversations.js
+++ client/src/components/Conversations.js
@@ -1,10 +1,38 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {ListGroup} from "react-bootstrap";
 
 const Conversations = () => {
+  // mock data
+  const [conversations, setConversations] = useState([
+    {selected: false, recipients: [{name: "test"}, {name: "test2"}]},
+    {selected: false, recipients: [{name: "test3"}]},
+    {selected: false, recipients: [{name: "test4"}]},
+  ])
+
+  const [selectedConversationIndex, setSelectedConversationIndex] = useState(0)
+
+  useEffect(() => {
+    setConversations(prevState => {
+      prevState.map((v, conversationIndex) => {
+        v.selected = conversationIndex === selectedConversationIndex
+        return v
+      })
+      return [...prevState]
+    })
+  }, [selectedConversationIndex])
+
   return (
-    <div>
-      トークルーム
-    </div>
+    <ListGroup variant="flush">
+      {conversations.map((conversation, index) => (
+        <ListGroup.Item
+          key={index}
+          onClick={() => setSelectedConversationIndex(index)}
+          active={conversation.selected}
+        >
+          {conversation.recipients.map(recipient => recipient.name).join(", ")}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
   );
 };
 
