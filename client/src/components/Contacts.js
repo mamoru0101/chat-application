1905f569e1e923df5bd271e952858e71f80f4810
kdm0101 <d08020386730@gmail.com>
2021-07-15 08:58:16 +0900

    contacts components

--- client/src/components/Contacts.js
+++ client/src/components/Contacts.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {ListGroup} from "react-bootstrap";
 
 const Contacts = () => {
+  // mockData
+  const contacts = [{id: "test_id", name: "test"}, {id: "test2_id", name: "test2"}]
+
   return (
-    <div>
-      連絡先
-    </div>
+    <ListGroup>
+      {contacts.map(contact => (
+        <ListGroup.Item key={contact.id}>
+          {contact.name}
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
   );
 };
 
